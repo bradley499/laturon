f9d9162ca02246a15ee47d3e237f320a20581a37
Bradley Marshall <bradm499@gmail.com>
2022-02-15 16:14:50 +0000

    Added support for automated definition countings with enumerations

--- interpreter/include/scope.h
+++ interpreter/include/scope.h
@@ -3,49 +3,62 @@
 
 #include "stack.h"
 
-#define SCOPE_SUCCESS 0				  // Successful operation
-#define SCOPE_FAILURE 1				  // Failed operation
-#define SCOPE_CREATION_FAILURE 2	  // Failed to allocate memory for new scope
-#define SCOPE_UNDEFINED 3			  // Scope has not been configured
-#define SCOPE_AWAIT_READY 4			  // Scope is await has not been declared
-#define SCOPE_AWAITING 5			  // Scope is awaiting primary completion
-#define SCOPE_AWAIT_FINISHED 6		  // Scope operations have finished
-#define SCOPE_AWAITING_CONCLUSION 7	  // Scope is awaiting secondary completion
-#define SCOPE_AWAIT_NO 8			  // Scope does not require completion awaits
-#define SCOPE_AWAITING_DELETION 9	  // Scope declaration for destruction
-#define SCOPE_TYPE_CONTAINER 10		  // Scope contains other general scopes
-#define SCOPE_TYPE_IF_CONTAINER 11	  // Scope is for an if container declaration
-#define SCOPE_TYPE_IF_ELSE 12		  // Scope is for an if, else declaration
-#define SCOPE_TYPE_INT 13			  // Scope is integer declaration
-#define SCOPE_TYPE_BOOL 14			  // Scope is boolean declaration
-#define SCOPE_TYPE_DOUBLE 15		  // Scope is double declaration
-#define SCOPE_TYPE_STRING 16		  // Scope is string declaration
-#define SCOPE_TYPE_FUNCTION 17		  // Scope is a function declaration
-#define SCOPE_TYPE_FUNCTION_CALL 18	  // Scope is a function call
-#define SCOPE_TYPE_PARAMETER_START 19 // Scope is a declarting the start of a parameter chain
-#define SCOPE_TYPE_PARAMETER_END 20	  // Scope is a declarting the end of a parameter chain
-#define SCOPE_TYPE_ARRAY 21			  // Scope is a array declaration
-#define SCOPE_TYPE_ARRAY_AT 22		  // Scope is a array value at position declaration
-#define SCOPE_TYPE_CONSTANT_INT 23	  // Scope is a constant declaration of a double
-#define SCOPE_TYPE_CONSTANT_DOUBLE 24 // Scope is a constant declaration of a double
-#define SCOPE_TYPE_CONSTANT_BOOL 25	  // Scope is a constant declaration of a boolean
-#define SCOPE_TYPE_CONSTANT_ARRAY 26  // Scope is a constant declaration of an array
-#define SCOPE_VARIABLE 27			  // Scope a variable
-#define SCOPE_VARIABLE_UPDATE 28	  // Scope is updating a variable
-#define SCOPE_LOGIC_EQUAL 29		  // Scope logic: equal
-#define SCOPE_LOGIC_NOT 30			  // Scope logic: not
-#define SCOPE_LOGIC_MORE 31			  // Scope logic: more
-#define SCOPE_LOGIC_MORE_OR_EQUAL 32  // Scope logic: more or equal
-#define SCOPE_LOGIC_LESS 33			  // Scope logic: less
-#define SCOPE_LOGIC_LESS_OR_EQUAL 34  // Scope logic: less or equal
-#define SCOPE_LOGIC_AND 35			  // Scope logic: and
-#define SCOPE_MATH_ADD 36			  // Scope math: add
-#define SCOPE_MATH_SUBTRACT 37		  // Scope math: substract
-#define SCOPE_MATH_MULTIPLY 38		  // Scope math: multiply
-#define SCOPE_MATH_DIVIDE 39		  // Scope math: divide
-#define SCOPE_MATH_MODULO 40		  // Scope math: divide
-#define SCOPE_BOOLEAN_TRUE 1.0		  // Scope boolean true
-#define SCOPE_BOOLEAN_FALSE 0.0		  // Scope boolean false
+#define SCOPE_SUCCESS 0				// Successful operation
+#define SCOPE_FAILURE 1				// Failed operation
+#define SCOPE_CREATION_FAILURE 2	// Failed to allocate memory for new scope
+#define SCOPE_UNDEFINED 3			// Scope has not been configured
+#define SCOPE_AWAIT_READY 4			// Scope is await has not been declared
+#define SCOPE_AWAITING 5			// Scope is awaiting primary completion
+#define SCOPE_AWAIT_FINISHED 6		// Scope operations have finished
+#define SCOPE_AWAITING_CONCLUSION 7 // Scope is awaiting secondary completion
+#define SCOPE_AWAIT_NO 8			// Scope does not require completion awaits
+#define SCOPE_AWAITING_DELETION 9	// Scope declaration for destruction
+typedef enum scope_types
+{
+	SCOPE_TYPE_CONTAINER = (SCOPE_AWAITING_DELETION + 1), // Scope contains other general scopes
+	SCOPE_TYPE_IF_CONTAINER,							  // Scope is for an if container declaration
+	SCOPE_TYPE_IF_ELSE,									  // Scope is for an if, else declaration
+	SCOPE_TYPE_INT,										  // Scope is integer declaration
+	SCOPE_TYPE_BOOL,									  // Scope is boolean declaration
+	SCOPE_TYPE_DOUBLE,									  // Scope is double declaration
+	SCOPE_TYPE_STRING,									  // Scope is string declaration
+	SCOPE_TYPE_STRING_CONTINUED,						  // Scope is string declaration
+	SCOPE_TYPE_FUNCTION,								  // Scope is a function declaration
+	SCOPE_TYPE_FUNCTION_CALL,							  // Scope is a function call
+	SCOPE_TYPE_PARAMETER_START,							  // Scope is a declarting the start of a parameter chain
+	SCOPE_TYPE_PARAMETER_END,							  // Scope is a declarting the end of a parameter chain
+	SCOPE_TYPE_ARRAY,									  // Scope is a array declaration
+	SCOPE_TYPE_ARRAY_AT,								  // Scope is a array value at position declaration
+	SCOPE_TYPE_CONSTANT_INT,							  // Scope is a constant declaration of a double
+	SCOPE_TYPE_CONSTANT_DOUBLE,							  // Scope is a constant declaration of a double
+	SCOPE_TYPE_CONSTANT_BOOL,							  // Scope is a constant declaration of a boolean
+	SCOPE_TYPE_CONSTANT_ARRAY,							  // Scope is a constant declaration of an array
+} scope_types;
+typedef enum scope_variable_states
+{
+	SCOPE_VARIABLE = (SCOPE_TYPE_CONSTANT_ARRAY + 1), // Scope a variable
+	SCOPE_VARIABLE_UPDATE,							  // Scope is updating a variable
+} scope_variable_states;
+typedef enum scope_logic
+{
+	SCOPE_LOGIC_EQUAL = (SCOPE_VARIABLE_UPDATE + 1), // Scope logic: equal
+	SCOPE_LOGIC_NOT,								 // Scope logic: not
+	SCOPE_LOGIC_MORE,								 // Scope logic: more
+	SCOPE_LOGIC_MORE_OR_EQUAL,						 // Scope logic: more or equal
+	SCOPE_LOGIC_LESS,								 // Scope logic: less
+	SCOPE_LOGIC_LESS_OR_EQUAL,						 // Scope logic: less or equal
+	SCOPE_LOGIC_AND,								 // Scope logic: and
+} scope_logic;
+typedef enum scope_math
+{
+	SCOPE_MATH_ADD = (SCOPE_LOGIC_AND + 1), // Scope math: add
+	SCOPE_MATH_SUBTRACT,					// Scope math: substract
+	SCOPE_MATH_MULTIPLY,					// Scope math: multiply
+	SCOPE_MATH_DIVIDE,						// Scope math: divide
+	SCOPE_MATH_MODULO,						// Scope math: divide
+} scope_math;
+#define SCOPE_BOOLEAN_TRUE 1.0	// Scope boolean true
+#define SCOPE_BOOLEAN_FALSE 0.0 // Scope boolean false
 #define SCOPE_DEFAULT_VALUE SCOPE_BOOLEAN_FALSE
 
 // Scope structure

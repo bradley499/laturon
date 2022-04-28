4ae7b05fa300da7840725e638e142ed801bb0bd4
bradley499 <55873758+bradley499@users.noreply.github.com>
2022-04-28 18:20:42 +0000

    Update www to output generated at 4e2cae0

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -16,14 +16,14 @@
 				}
 				return false;
 			}
-			if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+			if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
 				return false;
 			}
 			if (navigator.userAgent.match(/Chrome\/\d+/) !== null) {
 				const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
 				return raw ? (parseInt(raw[2], 10) > 85) : false;
 			}
-		} catch {}
+		} catch { }
 		return false;
 	})();
 	let interpreterData = null;
@@ -36,10 +36,10 @@
 	const loading = document.createElement("div");
 	const interacts = [document.createElement({ true: "div", false: "textarea" }[chromium]), document.createElement("div")];
 	interacts[0].setAttribute("spellcheck", false);
-	interacts[0].setAttribute("autocorrect","off");
-	interacts[0].setAttribute("autocapitalize","none");
-	interacts[0].setAttribute("autocomplete","off");
-	const buttons = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")];
+	interacts[0].setAttribute("autocorrect", "off");
+	interacts[0].setAttribute("autocapitalize", "none");
+	interacts[0].setAttribute("autocomplete", "off");
+	const buttons = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")];
 	loading.id = "loading";
 	let authorPictureBlob = null;
 	const authorPicture = await fetch("https://api.github.com/users/bradley499").then(response => response.json()).then(async data => {
@@ -164,7 +164,7 @@
 	loadingState(null, false);
 	window.onresize = () => {
 		if (buttonData[2]["state"] == 3) {
-			if (window.innerWidth > 600){
+			if (window.innerWidth > 600) {
 				changeExecutionState(null);
 			}
 		}
@@ -183,7 +183,17 @@
 			if (initialExecution) {
 				alertBuilder("Unable to run", "Sorry, but the interpreter is still being loaded... Until the interpreter has loaded, you'll have to wait before you can run your program.", null, null);
 			} else {
-				alertBuilder("Unable to run", "Sorry, but a new instance of the interpreter is still being loaded... Until this new instance of the interpreter has loaded, you'll have to wait before you can run your program.", null, null);
+				buttons[3].style.display = "inline-block";
+				buttons[2].style.display = "none";
+				await new Promise((resolve) => {
+					setInterval(() => {
+						if (interpreterReady) {
+							resolve();
+						}
+					}, 300);
+				});
+				buttons[3].style.display = "none";
+				buttons[2].style.display = "inline-block";
 			}
 			return;
 		}
@@ -218,7 +228,7 @@
 			}
 			newWorker(interpreterData, true);
 		}
-		if (buttonData[2]["state"] == 3){
+		if (buttonData[2]["state"] == 3) {
 			buttons[2].classList.remove("back");
 			buttons[2].classList.remove("executing");
 			interactsContainer.classList.remove("executing");
@@ -253,7 +263,7 @@
 		}
 		let element = document.createElement("input");
 		element.type = "file";
-		element.accept=".lt";
+		element.accept = ".lt";
 		element.addEventListener("change", async () => {
 			if (element.files.length == 0) {
 				return;
@@ -370,7 +380,7 @@
 	};
 	const buttonContainer = document.createElement("div");
 	buttonContainer.id = "buttonContainer";
-	const buttonData = [{ "id": "load", "tooltip": ["Load source code from file"], "function": load, "state": 0 }, { "id": "save", "tooltip": ["Download source code", "Downloading source code"], "function": save, "state": 0 }, { "id": "start_stop", "tooltip": ["Run program", "Stop program"], "function": changeExecutionState, "state": 0 }, { "id": "resize", "tooltip": ["Resize editor"], "function": resizeEditor, "state": 0 }]
+	const buttonData = [{ "id": "load", "tooltip": ["Load source code from file"], "function": load, "state": 0 }, { "id": "save", "tooltip": ["Download source code", "Downloading source code"], "function": save, "state": 0 }, { "id": "start_stop", "tooltip": ["Run program", "Stop program"], "function": changeExecutionState, "state": 0 }, { "id": "loadingSpinner", "tooltip": ["Loading..."], "function": () => { }, "state": 0 }, { "id": "resize", "tooltip": ["Resize editor"], "function": resizeEditor, "state": 0 }]
 	for (let i = 0; i < buttons.length; i++) {
 		buttons[i].id = buttonData[i]["id"];
 		buttons[i].title = buttonData[i]["tooltip"][0];
@@ -381,6 +391,10 @@
 		buttons[i].addEventListener("click", buttonData[i]["function"]);
 		buttonContainer.appendChild(buttons[i]);
 	}
+	const spinner = document.createElement("div");
+	spinner.id = "loadingSpinner";
+	buttons[3].appendChild(spinner);
+	buttons[3].style.display = "none";
 	interactsContainer.id = "interactsContainer";
 	interactsContainer.classList.add("editorSizing5050");
 	interacts[0].id = "editor";
@@ -539,8 +553,28 @@
 		if (interpreterData === false) {
 			buttons[2].remove();
 			buttons[3].remove();
+			buttons[4].remove();
 			resizeEditor();
 		}
-		newWorker(interpreterData);
+		const supportsWebAssembly = (() => {
+			try {
+				if (typeof WebAssembly === "object"
+					&& typeof WebAssembly.instantiate === "function") {
+					const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+					if (module instanceof WebAssembly.Module)
+						return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+				}
+			} catch (e) {}
+			return false;
+		})();
+		if (!window.Worker || !supportsWebAssembly) {
+			alertBuilder("Browser not supported", "Sorry, but your browser is not supported due to it have missing (or disabled) features required to run the Laturon IDE.", null, null);
+			buttons[2].remove();
+			buttons[3].remove();
+			buttons[4].remove();
+			resizeEditor();
+		} else {
+			newWorker(interpreterData);
+		}
 	})();
 })();
